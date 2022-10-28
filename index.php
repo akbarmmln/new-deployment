86b69020b0b420870b139da13970b943e886ebf5
Taufik <muhammad.taufik@adira.co.id>
2022-10-28 14:56:41 +0700

    update code

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<?php
+    <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     include('/Net/SSH2.php');
-    $ssh = new Net_SSH2('103.152.118.253');
+    $ssh = new Net_SSH2('103.152.118.253', 22);
     if (!$ssh->login('root', 'CYGr%@dOYHc0')) {
         echo "Login Failed";
     }
