5eb116bf7303159849fcac03124ad8855bf814b9
Taufik <muhammad.taufik@adira.co.id>
2022-10-28 14:53:00 +0700

    update code

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-    echo "halo ini saya";
+    include('/phpseclib/Net/SSH2.php');
+    $ssh = new Net_SSH2('103.152.118.253');
+    if (!$ssh->login('root', 'CYGr%@dOYHc0')) {
+        exit('Login Failed');
+    }
+    echo $ssh->exec('ls -la');
+    echo $ssh->exec('logout');
 ?>
