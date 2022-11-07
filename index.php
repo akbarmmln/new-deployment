afb1db1caed4d3284e20b70402f651b94b89bd21
Taufik <muhammad.taufik@adira.co.id>
2022-11-07 16:21:06 +0700

    update code

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    // $ms = $_GET['ms'];
-    // echo $ms;
-    // echo "<br>";
+    $ms = $_GET['ms'];
+    echo $ms;
+    echo "<br>";
     // include('Net/SSH2.php');
     // $ssh = new Net_SSH2('103.152.118.253', 22);
     // if (!$ssh->login('root', 'CYGr%@dOYHc0')) {
@@ -14,14 +14,14 @@
 
     // echo $ssh->exec('cd prjct/"'.$ms.'"; ls');
     // echo $ssh->exec('exit');
-
+    
     $connection = ssh2_connect('103.152.118.253', 22);
     ssh2_auth_password($connection, 'root', 'CYGr%@dOYHc0');
 
     echo "step: masuk ke lokasi project <br>";
-    $step1 = ssh2_exec($connection, 'cd microservices/4yur-dev-ms-one');
-    $sio_step1 = ssh2_fetch_stream($tahap1, SSH2_STREAM_STDIO);
-    $err_step1 = ssh2_fetch_stream($tahap1, SSH2_STREAM_STDERR);
+    $step1 = ssh2_exec($connection, 'cd microservices/"'.$ms.'"');
+    $sio_step1 = ssh2_fetch_stream($step1, SSH2_STREAM_STDIO);
+    $err_step1 = ssh2_fetch_stream($step1, SSH2_STREAM_STDERR);
 
     stream_set_blocking($sio_step1, true);
     stream_set_blocking($err_step1, true);
@@ -35,8 +35,8 @@
 
     echo "step: list folder project <br>";
     $step2 = ssh2_exec($connection, 'ls');
-    $sio_step2 = ssh2_fetch_stream($tahap2, SSH2_STREAM_STDIO);
-    $err_step2 = ssh2_fetch_stream($tahap2, SSH2_STREAM_STDERR);
+    $sio_step2 = ssh2_fetch_stream($step2, SSH2_STREAM_STDIO);
+    $err_step2 = ssh2_fetch_stream($step2, SSH2_STREAM_STDERR);
 
     stream_set_blocking($sio_step2, true);
     stream_set_blocking($err_step2, true);
