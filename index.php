637d5422afa5f1c8cb1afe81969eb3b095c42553
Taufik <muhammad.taufik@adira.co.id>
2022-11-07 10:01:29 +0700

    update code

--- index.php
+++ index.php
@@ -18,5 +18,16 @@
     $connection = ssh2_connect('103.152.118.253', 22);
     ssh2_auth_password($connection, 'root', 'CYGr%@dOYHc0');
     $stream = ssh2_exec($connection, 'ls');
-    echo $stream;
+    
+    $sio_stream = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+    $err_stream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
+
+    stream_set_blocking($sio_stream, true);
+    stream_set_blocking($err_stream, true);
+
+    $result_dio = stream_get_contents($sio_stream);
+    $result_err = stream_get_contents($err_stream);
+
+    echo 'stderr: ' . $result_err;
+    echo 'stdio : ' . $result_dio;
 ?>
