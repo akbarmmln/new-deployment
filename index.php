7b73b0c8646fc63cad792cbfa03e84b4f99a6f46
Taufik <muhammad.taufik@adira.co.id>
2022-11-07 16:16:20 +0700

    update code

--- index.php
+++ index.php
@@ -17,18 +17,35 @@
 
     $connection = ssh2_connect('103.152.118.253', 22);
     ssh2_auth_password($connection, 'root', 'CYGr%@dOYHc0');
-    $stream = ssh2_exec($connection, 'ls');
-    
-    $sio_stream = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
-    $err_stream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
 
-    stream_set_blocking($sio_stream, true);
-    stream_set_blocking($err_stream, true);
+    echo "step: masuk ke lokasi project <br>";
+    $step1 = ssh2_exec($connection, 'cd microservices/4yur-dev-ms-one');
+    $sio_step1 = ssh2_fetch_stream($tahap1, SSH2_STREAM_STDIO);
+    $err_step1 = ssh2_fetch_stream($tahap1, SSH2_STREAM_STDERR);
 
-    $result_dio = stream_get_contents($sio_stream);
-    $result_err = stream_get_contents($err_stream);
+    stream_set_blocking($sio_step1, true);
+    stream_set_blocking($err_step1, true);
 
-    echo 'stderr: ' . $result_err;
+    $result_dio_step1 = stream_get_contents($sio_step1);
+    $result_err_step1 = stream_get_contents($err_step1);
+
+    echo 'stdio : ' . $result_dio_step1;
     echo "<br>";
-    echo 'stdio : ' . $result_dio;
+    echo 'stderr: ' . $result_err_step1;
+
+    echo "step: list folder project <br>";
+    $step2 = ssh2_exec($connection, 'ls');
+    $sio_step2 = ssh2_fetch_stream($tahap2, SSH2_STREAM_STDIO);
+    $err_step2 = ssh2_fetch_stream($tahap2, SSH2_STREAM_STDERR);
+
+    stream_set_blocking($sio_step2, true);
+    stream_set_blocking($err_step2, true);
+
+    $result_dio_step2 = stream_get_contents($sio_step2);
+    $result_err_step2 = stream_get_contents($err_step2);
+
+    echo 'stdio : ' . $result_dio_step2;
+    echo "<br>";
+    echo 'stderr: ' . $result_err_step2;
+
 ?>
